d93ed6814909af7b58643566349f69a4c3e14afe
charlsoluwapelumi <charlsoluwapelumi@gmail.com>
2024-05-14 09:03:00 +0100

    stripe

--- download.php
+++ download.php
@@ -33,8 +33,8 @@
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'snscharmie@gmail.com';                     //SMTP username
-        $mail->Password   = 'xamh erty gkva okgd';                               //SMTP password
+        $mail->Username   = 'example@gmail.com';                     //SMTP username
+        $mail->Password   = 'type your pass word';                               //SMTP password
         // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
